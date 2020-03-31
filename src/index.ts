bd9989a5e21db19031ed22fe8b50632f9de53ab8
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-31 16:51:53 +0800

    feat: Add apollo-server boilerplate code

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from 'apollo-server';
+
+import schema from './schema';
+import resolvers from './resolvers';
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
