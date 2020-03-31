ec0fd24cd28b9a1d0edbaff3666fde762cac12f3
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-03-31 18:21:26 +0800

    feat: Load config from .env file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
+import dotenv from 'dotenv';
 import { ApolloServer } from 'apollo-server';
 
 import schema from './schema';
 import resolvers from './resolvers';
+
+dotenv.config();
+if (!process.env.DB_NAME) {
+  throw Error(
+    'Did not find DB_NAME environment variable. Did you set it in .env file?',
+  );
+}
+if (!process.env.DB_ADDRESS) {
+  throw Error(
+    'Did not find DB_ADDRESS environment variable. Did you set it in .env file?',
+  );
+}
 
 const server = new ApolloServer({
   typeDefs: schema,
