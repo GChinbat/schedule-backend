dc818b68d9acf7b3abb0608101f898d6facc7ee9
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-01 01:09:10 +0800

    feat: Add resolver for ScheduleItem.id

--- src/resolvers/fields/scheduleItem.ts
+++ src/resolvers/fields/scheduleItem.ts
@@ -2,6 +2,7 @@ import * as lessonGroupModel from '@/models/lessonGroup';
 import { ScheduleItem } from '@/models/scheduleItem';
 
 export default {
+  id: (item: ScheduleItem) => item._id.toHexString(),
   lessonGroup: async (item: ScheduleItem) =>
     lessonGroupModel.getGroup(item.lessonGroup),
 };
