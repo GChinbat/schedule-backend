92f48e909f85acc4c69a1cb6aa60741a6bc9e90d
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-01 00:34:02 +0800

    feat(model): Schedule item

---
+++ src/models/scheduleItem.ts
@@ -0,0 +1,91 @@
+import { ObjectID } from 'mongodb';
+
+import { db } from './init';
+import { LessonGroup } from './lessonGroup';
+
+export type ScheduleItem = {
+  _id?: ObjectID;
+  day: number;
+  endTime: number;
+  startTime: number;
+  lessonGroup: ObjectID;
+};
+
+export type ScheduleItemInput = {
+  day: number;
+  endTime: number;
+  startTime: number;
+  groupSlug: string;
+};
+export type EditScheduleItemInput = Partial<
+  Omit<ScheduleItemInput, 'groupSlug'>
+>;
+
+export async function getSchedule() {
+  const scheduleCollection = db.collection<ScheduleItem>('schedule');
+  const scheduleItems = await scheduleCollection.find().toArray();
+
+  const results: ScheduleItem[][] = Array(5).fill([]);
+  scheduleItems.forEach((item) => results[item.day - 1].push(item));
+
+  return results;
+}
+
+export async function getScheduleItem(
+  id: ObjectID,
+): Promise<ScheduleItem | null> {
+  const scheduleCollection = db.collection<ScheduleItem>('schedule');
+  return (await scheduleCollection.findOne({ _id: id })) ?? null;
+}
+
+export async function addScheduleItem({
+  day,
+  endTime,
+  startTime,
+  groupSlug,
+}: ScheduleItemInput) {
+  const groupsCollection = db.collection<LessonGroup>('lessonGroups');
+  const group = await groupsCollection.findOne({ slug: groupSlug });
+  if (!group) {
+    throw Error(`Lesson group ${groupSlug} was not found!`);
+  }
+
+  const scheduleItem: ScheduleItem = {
+    day,
+    endTime,
+    startTime,
+    lessonGroup: group._id,
+  };
+  const scheduleCollection = db.collection<ScheduleItem>('schedule');
+  scheduleItem._id = (
+    await scheduleCollection.insertOne(scheduleItem)
+  ).insertedId;
+
+  return scheduleItem;
+}
+
+export async function editScheduleItem(
+  id: string,
+  item: EditScheduleItemInput,
+) {
+  const scheduleCollection = db.collection<ScheduleItem>('schedule');
+
+  const _id = ObjectID.createFromHexString(id);
+  if ((await scheduleCollection.find({ _id }).count()) === 0) {
+    throw Error(`Schedule item with ID ${id} was not found!`);
+  }
+
+  return (
+    await scheduleCollection.findOneAndUpdate(
+      { _id },
+      { $set: item },
+      { returnOriginal: false },
+    )
+  ).value;
+}
+
+export async function removeScheduleItem(id: string) {
+  const scheduleCollection = db.collection<ScheduleItem>('schedule');
+  await scheduleCollection.deleteOne({ _id: ObjectID.createFromHexString(id) });
+  return true;
+}
